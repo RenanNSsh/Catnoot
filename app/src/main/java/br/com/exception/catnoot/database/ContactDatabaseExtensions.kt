0d3e2e1f1378500529001861864feb00727318f0
Renan do Nascimento Sanches <renan.sanches_123@hotmail.com>
2020-06-03 20:38:31 -0300

    Repository e extensions

---
+++ app/src/main/java/br/com/exception/catnoot/database/ContactDatabaseExtensions.kt
@@ -0,0 +1,15 @@
+package br.com.exception.catnoot.database
+
+import br.com.exception.catnoot.database.ContactDatabase
+
+fun ContactDatabase.listContacts() {
+}
+
+fun ContactDatabase.addContact() {
+}
+
+fun ContactDatabase.removeContact() {
+}
+
+fun ContactDatabase.updateContact() {
+}
--- app/src/main/java/br/com/exception/catnoot/screens/ContactAddFragment.kt
+++ app/src/main/java/br/com/exception/catnoot/screens/ContactAddFragment.kt
@@ -1,4 +1,4 @@
-package br.com.exception.catnoot
+package br.com.exception.catnoot.screens
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import br.com.exception.catnoot.R
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
