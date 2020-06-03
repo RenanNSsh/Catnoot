d3f8f886e3b5aa514c26ff53ed53cd1dac0362f8
Chabarys <alissonchabaribery6@gmail.com>
2020-06-03 20:30:50 -0300

    Classe model de contatos

--- app/src/main/java/br/com/exception/catnoot/model/Contact.kt
+++ app/src/main/java/br/com/exception/catnoot/model/Contact.kt
@@ -1,7 +1,5 @@
-data class Tarefa(
+data class Contact(
         val contactId: Int,
         var contactName: String,
         var contactPhoneNumber: String,
-        var contactEmail: String? = null,
-        var description: String? = null
 )
